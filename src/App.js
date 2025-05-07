44cb38a90fb6b3690c5092552bfaaa228780a65e
KPkm25 <parakramkumar22@gmail.com>
2025-05-08 00:49:06 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CustomerList from './components/CustomerList';
+import CustomerForm from './components/CustomerForm';
+import HealthCheck from './components/HealthCheck';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                <Route path="/" element={<HealthCheck />} /> 
+                    <Route path="/customers" element={<CustomerList />} />
+                    <Route path="/customers/new" element={<CustomerForm />} />
+                    <Route path="/edit/:id" element={<CustomerForm />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
